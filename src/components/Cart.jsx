498047d8256fa46b51353a431c8060444044ce17
Louis <colletlouis@gmail.com>
2021-10-04 15:41:50 +0200

    Added state to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Cart.css'
 
 function Cart() {
   const monsteraPrice = 8
-  const ivyPrice = 10
-  const flowerPrice = 15
-  return (
+  const [cart, updateCart] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
+
+  return isOpen ? (
     <div className="lmj-cart">
+      <button
+        className="lmj-cart-toggle-button"
+        onClick={() => setIsOpen(false)}
+        type="button"
+      >
+        Fermer le Panier
+      </button>
       <h2>Panier</h2>
-      <ul>
-        <li>
-          Monstera :
-          {monsteraPrice}
-          €
-        </li>
-        <li>
-          Lierre :
-          {ivyPrice}
-          €
-        </li>
-        <li>
-          Fleurs :
-          {flowerPrice}
-          €
-        </li>
-      </ul>
-      Total :
-      {' '}
-      {monsteraPrice + ivyPrice + flowerPrice}
-      €
+      <div>
+        Monstera :
+        {' '}
+        {monsteraPrice}
+        €
+        <button onClick={() => updateCart(cart + 1)} type="button">
+          Ajouter
+        </button>
+      </div>
+      <h3>
+        Total :
+        {' '}
+        {monsteraPrice * cart}
+        €
+      </h3>
+      <button onClick={() => updateCart(0)} type="button">
+        Vider le panier
+      </button>
+    </div>
+  ) : (
+    <div className="lmj-cart-closed">
+      <button
+        className="lmj-cart-toggle-button"
+        onClick={() => setIsOpen(true)}
+        type="button"
+      >
+        Ouvrir le Panier
+
+      </button>
     </div>
   )
 }
